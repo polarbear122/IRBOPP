1f323104654e2d13e6a7d74517351a3d56176601
周云飞 <zhouyunfei0909@163.com>
2022-04-19 21:02:34 +0800

    fix bug

--- toolkit/XMLRead.py
+++ toolkit/XMLRead.py
@@ -1,4 +1,6 @@
 # 仅能读取小文件，注释超过300的文件会读取出错
+import logging
+
 import cv2
 import xmltodict
 
@@ -9,13 +11,23 @@ def XMLRead(xml_file_path, output_file, video_id):
         # xmltodict.parse()方法可以将xml数据转为python中的dict字典数据
         parser_data = xmltodict.parse(xml_file.read())
         annotations = parser_data["annotations"]
+        if "track" not in annotations:
+            logging.info(xml_file_path, "no track exist")
+            print(xml_file_path, "no track exist")
+            return
         if "box" in annotations["track"]:
             track_box = [annotations["track"]]
         else:
             track_box = annotations["track"]
+
+        is_repeat = 0
         for i in track_box:
             if i["@label"] != "pedestrian":
                 continue
+            is_repeat += 1
+            if is_repeat >= 2:
+                logging.info(xml_file_path, "there are two box in one video")
+                print(xml_file_path, "there are two box in one video")
             for annotation in i["box"]:
                 is_look = annotation["attribute"][2]["#text"]
                 xbr = str_to_int(annotation["@xbr"])  # 左上角，右下角 (top-left, bottom-right)
@@ -25,16 +37,16 @@ def XMLRead(xml_file_path, output_file, video_id):
                 x_mid, y_mid = (xtl + xbr) // 2, (ytl + ybr) // 2
 
                 img = cv2.imread(output_file + annotation["@frame"] + ".jpg")
-                print(output_file + annotation["@frame"] + ".jpg")
                 cv2.line(img, (xbr, ytl), (xtl, ytl), (0, 0, 255), thickness=2)
                 cv2.line(img, (xbr, ytl), (xbr, ybr), (0, 0, 255), thickness=2)
                 cv2.line(img, (xbr, ybr), (xtl, ybr), (0, 0, 255), thickness=2)
                 cv2.line(img, (xtl, ybr), (xtl, ytl), (0, 0, 255), thickness=2)
                 cv2.putText(img, is_look, (x_mid, y_mid), cv2.FONT_HERSHEY_SIMPLEX, 1, (55, 255, 155), 2)
                 # cv2.imwrite("./save.jpg", img)
-                cv2.resize(img, (1000, 500), )
+
+                img = cv2.resize(img, (1920 // 2, 1080 // 2))
                 cv2.imshow("img", img)
-                cv2.waitKey(10)
+                cv2.waitKey(1)
 
         xml_file.close()
 
@@ -45,10 +57,12 @@ def str_to_int(float_str):
 
 
 def main():
-    video_id = "video_0002"
-    xml_path = "E:/CodeResp/pycode/DataSet/JAAD-JAAD_2.0/annotations/" + video_id + ".xml"
-    output_data_path = "E:/CodeResp/pycode/DataSet/JAAD_image/" + video_id + "/"
-    XMLRead(xml_path, output_data_path, video_id)
+    for i in range(1, 300):
+        video_id = "video_" + str(i).zfill(4)
+        xml_path = "E:/CodeResp/pycode/DataSet/JAAD-JAAD_2.0/annotations/" + video_id + ".xml"
+        output_data_path = "E:/CodeResp/pycode/DataSet/JAAD_image/" + video_id + "/"
+        XMLRead(xml_path, output_data_path, video_id)
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
